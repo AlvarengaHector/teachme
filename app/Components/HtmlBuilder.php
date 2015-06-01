3428a113510f3691f340588da1ea4867413802c5
Hector Alvarenga <smarthealvarenga@gmail.com>
2015-05-31 22:28:52 -0600

    Create a component for generate Html

--- app/Components/HtmlBuilder.php
+++ app/Components/HtmlBuilder.php
@@ -1,11 +1,57 @@
 <?php namespace TeachMe\Components;
 
+use Illuminate\Contracts\Config\Repository as Config;
+use Illuminate\Contracts\View\Factory as View;
+use Illuminate\Routing\UrlGenerator;
 use Collective\Html\HtmlBuilder as CollectiveHtmlBuilder;
 
 class HtmlBuilder extends CollectiveHtmlBuilder
 {
-	public function menu()
+	private $config;
+	private $view;
+
+	function __construct(Config $config, View $view, UrlGenerator $url) {
+		$this->config = $config;
+		$this->view = $view;
+		$this->url = $url;
+	}
+	public function menu($items)
 	{
-		return view('partials/menu');
+
+		if ( ! is_array($items)) {
+			$items = $this->config->get($items, array());
+		}
+
+		return $this->view->make('partials/menu', compact('items'));
+	}
+
+	/**
+	 * Builds an Html class attribute dynamically
+	 * Usage:
+	 * {!! Html::classes(['home' => true, 'main', 'dont-use-this' => false]) !!}
+	 * Returns:
+	 *  class="home main".
+	 *
+	 * @param  array $classes
+	 *
+	 * @return string 
+	 */
+	
+	public function classes(array $classes)
+	{
+		$html = '';
+		foreach ($classes as $name => $bool) {
+			if (is_int($name)) {
+				$name = $bool;
+				$bool = true;
+			}
+			if ($bool) {
+				$html .= $name. ' ';
+			}
+		}
+		if ( ! empty($html)) {
+			return ' class="'.trim($html).'"';
+		}
+		return '';
 	}
 }
